3ccfb1ce9b08fb8b12772f820fce1294c0db057f
Elijas <master.elijas@gmail.com>
2015-01-16 22:46:36 +0200

    2 hidden layers, 100% accuracy

--- nn.m
+++ nn.m
@@ -1,6 +1,7 @@
 function nn
 addpath('nn')
 
+% Dataset
 X = (1:16)';
 y = [ ...
 0,0,0,1,0,0,1;% 1
@@ -21,11 +22,14 @@ y = [ ...
 1,1,1,0,1,0,0;% F
 ];
 
-nn_lambda = .0001;
-nn_lsizes = [size(X,2) 7];
-nn_options = optimset('MaxIter', 600);
-nn_params = fmincg(@(p) nnCostFunction(p, nn_lsizes, X, y, nn_lambda), nnInitParams(nn_lsizes), nn_options);
+% Training NN
+nn_lambda = 0.00001;
+nn_lsizes = [size(X,2) 20 20 7];
+nn_options = optimset('MaxIter', 4000);
+%nn_params = fmincg(@(p) nnCostFunction(p, nn_lsizes, X, y, nn_lambda), nnInitParams(nn_lsizes), nn_options);
+load trained.nnweights %used instead of the line above, to skip training
 
+% Printing weights (by unrolling Theta matrices (weights) from parameter vector)
 %{
 params_pos=0;
 for i=1:size(nn_lsizes,2)-1;
@@ -35,6 +39,7 @@ for i=1:size(nn_lsizes,2)-1;
 end
 %}
 
+% Results
 for i=X'
     printf('input: %d expected output:\n',i)
     disp(y(i,:))
@@ -44,7 +49,6 @@ for i=X'
     print_output(round(nnFeedForward(nn_params, nn_lsizes, i)))
     printf('\n\n\n');
 end
-
 accuracy = mean(  (  y==round(nnFeedForward(nn_params, nn_lsizes, X))  )(:)  );
 printf('layers: '), printf(' %d', nn_lsizes), printf('\t| accuracy: %10.2f%%\n', accuracy*100);
 
